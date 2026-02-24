37fd28e5a2d53b940a13994515b1ecbcf9d04eca
Sibsankar-de <sibsankarde910@gmail.com>
2026-02-24 15:57:24 +0530

    feat: #8 Db URI fixed.

--- gsecure/lib/db/mongodb.js
+++ gsecure/lib/db/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectingtoDB = async()=>{
     try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/gsecure?authSource=admin`)
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/gsecure`)
         console.log(`MongoDB connected At HOST :: ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("Failed to connect to MONGODB : ",error);
